d2dd68adbebb336472240e5d9a4acd2076b0511d
Rahul Mittal <rahulmittaljuly2@gmail.com>
2024-11-12 00:32:20 +0530

    Initial Commit

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,13 @@
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
+var app = builder.Build();
+
+app.MapGamesEndpoint();
+
+app.Run();
